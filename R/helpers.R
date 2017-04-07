44b7bbe3b4fc44f194e74f3625ed13a46594dbdd
jpmarindiaz <jpmarindiaz@gmail.com>
2017-04-07 10:43:19 -0500

    move_first export

--- R/helpers.R
+++ R/helpers.R
@@ -1,8 +1,10 @@
 
+#' @export
 move_first <- function(d,name){
   d[c(name, names(d)[!names(d) %in% name])]
 }
 
+#' @export
 nested_to_list <- function(d){
   d <- d %>% arrange_(.dots=names(d)[1])
   l <- d[[2]]
